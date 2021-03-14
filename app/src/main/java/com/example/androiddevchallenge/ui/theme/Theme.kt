b7a9f10d32461fd4265411db19353e56c4f68431
ruger.crackme <550279039@qq.com>
2021-03-14 17:50:51 +0800

    feat: develop welcome page

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
@@ -15,11 +15,41 @@
  */
 package com.example.androiddevchallenge.ui.theme
 
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.example.androiddevchallenge.R
+
+
+
+private val BloomLightColorPaltte = lightColors(
+    primary = pink100,
+    secondary = pink900,
+    background = white,
+    surface = white850,
+    onPrimary = gray,
+    onSecondary = white,
+    onBackground = gray,
+    onSurface = gray,
+)
+
+private val BloomDarkColorPaltte = darkColors(
+    primary = green900,
+    secondary = green300,
+    background = gray,
+    surface = white150,
+    onPrimary = white,
+    onSecondary = gray,
+    onBackground = white,
+    onSurface = white850
+)
+
+
 
 private val DarkColorPalette = darkColors(
     primary = purple200,
@@ -53,6 +83,47 @@ fun MyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() (
     MaterialTheme(
         colors = colors,
         typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
+
+object WelcomeAssets {
+    var background: Int = 0
+    var illos: Int = 0
+    var logo: Int = 0
+    fun set(background: Int, illos: Int, logo: Int) {
+        this.background = background
+        this.illos = illos
+        this.logo = logo
+    }
+}
+
+val MaterialTheme.welcomeAssets: WelcomeAssets
+    get() = WelcomeAssets
+
+@Composable
+fun BloomTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    var colors: Colors
+    if (darkTheme) {
+        colors = BloomDarkColorPaltte
+        WelcomeAssets.set(
+            background = R.drawable.ic_dark_welcome_bg,
+            illos = R.drawable.ic_dark_welcome_illos,
+            logo = R.drawable.ic_dark_logo
+        )
+    } else {
+        colors = BloomLightColorPaltte
+        WelcomeAssets.set(
+            background = R.drawable.ic_light_welcome_bg,
+            illos = R.drawable.ic_light_welcome_illos,
+            logo = R.drawable.ic_light_logo
+        )
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = bloomTypoGraphy,
         shapes = shapes,
         content = content
     )
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,9 +17,11 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
 
 // Set of Material typography styles to start with
 val typography = Typography(
@@ -28,16 +30,51 @@ val typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-        /* Other default text styles to override
+)
+
+val nunitoSansFamily = FontFamily(
+    Font(R.font.nunitosans_light, FontWeight.Light),
+    Font(R.font.nunitosans_semibold, FontWeight.SemiBold),
+    Font(R.font.nunitosans_bold, FontWeight.Bold)
+)
+
+val bloomTypoGraphy = Typography(
+    h1 = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Bold
+    ),
+    h2 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 15.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Bold
+    ),
+    subtitle1 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Light
+    ),
+    body1 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Light
+    ),
+    body2 = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Light
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 14.sp,
+        letterSpacing = 1.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.SemiBold,
+        color = white
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.SemiBold
     )
-    */
 )
